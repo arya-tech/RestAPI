0c92a43f0c5e9f4950d4a99d243856ebfaa23e36
Arya <aryadewan123@gmail.com>
2021-06-16 17:31:32 +0530

    added HTTP GET method

--- src/main/java/com/demo/controller/MangalController.java
+++ src/main/java/com/demo/controller/MangalController.java
@@ -1,10 +1,16 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mangal")
 public class MangalController {
+	
+	@GetMapping("/")
+	public String greet() {
+		return "hello mangal";
+	}
 
 }
